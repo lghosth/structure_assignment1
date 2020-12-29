b76764c74f573cf2bfe3d14db6baa5f6d64f1d37
浩 李 <penghao.lee@outlook.com>
2020-12-29 09:45:16 +0800

    Initial commit

---
+++ head.h
@@ -0,0 +1,11 @@
+//
+// Created by charol on 2020/12/29.
+//
+
+#ifndef STRUCTURE_ASSIGNMENT1_HEAD_H
+#define STRUCTURE_ASSIGNMENT1_HEAD_H
+#include <malloc.h>
+#include <stdlib.h>
+
+
+#endif //STRUCTURE_ASSIGNMENT1_HEAD_H
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "head.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
